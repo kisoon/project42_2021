4da6d11221bd4fc083ec5a3905d839995d5a0501
kisoonEom <kisoon1106@gmail.com>
2021-09-08 14:22:23 +0900

    Add files via upload

--- 210709_project42_esp32_UDP/NeoPixelLED.h
+++ 210709_project42_esp32_UDP/NeoPixelLED.h
@@ -17,48 +17,80 @@ uint32_t white = strip.Color(255, 255, 255);
 uint32_t black = strip.Color(0, 0, 0);
 
 uint32_t ledColor = red;
-int brightness = 20;
+int brightness = 255;
+
+unsigned long previousMillis = 0;
+int ledState = 0;
 
 void initLED() {
-
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(brightness); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-  }
-}
-
-
 void ledShow() {
-  for(int i=0; i<strip.numPixels(); i++) {
+  for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, ledColor);
   }
   strip.show();
 }
 
 void ledOff() {
-  for(int i=0; i<strip.numPixels(); i++) {
+  for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, black);
   }
   strip.show();
+}
+
+void ledBlink(int interval) {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (ledState == LOW) {
+      ledState = HIGH;
+
+    } else {
+      ledState = LOW;
+    }
+  }
+
+  if (ledState) {
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, ledColor);
+    }
+  } else {
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, black);
+    }
+    
+  }
+
+  strip.show();
+}
+
+void ledRainbow(int interval){
+  unsigned long currentMillis = millis();
+
+  static unsigned long firstPixelHue = 0;
+    if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    firstPixelHue += 256;
+    if (firstPixelHue >= 5*65536) {
+      firstPixelHue = 0;
+    }
+  }
+  
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    // Offset pixel hue by an amount to make one full revolution of the
+    // color wheel (range of 65536) along the length of the strip
+    // (strip.numPixels() steps):
+    int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+
+    strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+  }
+  strip.show(); // Update strip with new contents
+
 }
